e35985ed56bf8d0a851f1254707efbc05dbf89a5
a-farrow <austinfarrowbaseball@gmail.com>
2024-05-02 15:42:22 -0500

    Added contact form, edited multiple views such as Courses, Students, Contact Page, etc

---
+++ SAT/SAT.UI.MVC/Models/ContactViewModel.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+
+namespace SAT.UI.MVC.Models
+{
+    [Keyless]
+    public class ContactViewModel
+    {
+        [Required]
+        [MaxLength(100)]
+        public string Name { get; set; } = null!;
+
+        [Required]
+        [DataType(DataType.EmailAddress)]
+        public string Email { get; set; } = null!;
+
+        [Required]
+        [MaxLength(100)]
+        public string Subject { get; set; } = null!;
+
+        [Required]
+        [DataType(DataType.MultilineText)]
+        public string Message { get; set; } = null!;
+    }
+}
